4ce13f6d55d1ccd1637909b4a8cfbe743456d923
Cristina Kelly <cristinakellyt@gmail.com>
2024-12-10 21:38:27 +0100

    chore: clean vue boilerplate code and install sass

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
   </main>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+h1 {
+  color: $color-primary;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use '@/assets/scss/abstract/functions' as *;
+        @use '@/assets/scss/abstract/mixins' as *;
+        @use '@/assets/scss/abstract/variables' as *;
+        @use '@/assets/scss/base/base' as *;`,
+      },
     },
   },
 })
