d1a75252d9e93802b5bc3fabb07887935367be30
Cristina Kelly <cristinakellyt@gmail.com>
2024-12-11 07:50:52 +0100

    feat: create store management for public holidays and implement fetch for available countries

---
+++ src/stores/publicHolidaysStore.ts
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { Country } from '@/types/country'
+
+const API_URL = 'https://date.nager.at/api/v3/'
+
+export const usePublicHolidaysStore = defineStore('publicHolidays', () => {
+  const availableCountries = ref<Country[]>([])
+
+  const fetchAvailableCountries = async () => {
+    try {
+      const response = await fetch(`${API_URL}AvailableCountries`)
+      const data = await response.json()
+      availableCountries.value = data
+    } catch (error) {
+      throw new Error('Error fetching available countries')
+    }
+  }
+
+  return { availableCountries, fetchAvailableCountries }
+})
