4766ef095f72f623baa5cf4dbd944bc60545cbd9
Cristina Kelly <cristinakellyt@gmail.com>
2024-12-11 07:36:54 +0100

    feat: implement BaseWidth component

---
+++ src/components/BasicComponents/BaseWidth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="base-width">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/scss/main.scss';
+
+.base-width {
+  max-width: $max-width;
+  margin: 0 auto;
+  padding: pxToRem(32);
+}
+</style>
